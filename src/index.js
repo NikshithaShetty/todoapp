0563b7102910dfac6df0a35eba9b7cea871f8d93
Nikshitha Shetty <nikshitha.niks@gmail.com>
2023-06-14 16:59:33 +0530

    persist commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import store from './redux/store';
+import store ,{persistor} from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import ContextProvider from './context/ContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ContextProvider>
     <Provider store={store}>
+      < PersistGate persistor={persistor}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
+      </PersistGate>
     </Provider>
   </ContextProvider>);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,16 @@
 import { createStore } from "redux";
 import rootreducers from "./reducers/main";
+import { persistStore, persistReducer} from "redux-persist";
+import  storage  from "redux-persist/lib/storage";
 
-
+const persistConfig={
+key :"persistkey",
+storage
+}
+const persistedReducer = persistReducer(persistConfig,rootreducers)
 const store = createStore(
-    rootreducers
+    persistedReducer
 );
-
+const persistor= persistStore(store)
 export default store;
+export {persistor}
